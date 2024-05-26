e837978869223f711c546ff90f0af71fd1311688
MrtnDat25 <datgrb123456@gmail.com>
2024-05-26 13:49:30 +0700

    Test1

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors        = require('cors');
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
-
+  let helmet = require('helmet');
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -16,6 +16,18 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(helmet.contentSecurityPolicy
+    (
+      {
+        directives:
+          {
+            defaultSrc:["'self'"],
+            scriptSrc:["'self'"],
+            styleSrc:["self"],
+          }
+      }
+    )
+  )
 
 //Index page (static HTML)
 app.route('/')
